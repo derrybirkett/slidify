1f065bd50a9c944d60258140e717c01c659412e7
Derry Birkett <derry@d2tstudio.com>
2013-09-11 14:19:10 +0200

    First Commit

---
+++ jquery.slidify.js
@@ -0,0 +1,100 @@
+(function ( $ ) {
+ 
+ 	/*
+ 	
+ 		Creates a slider from a set of subelements
+ 			attach .slidify to slider element
+ 			
+ 			01 Create parent div as container
+ 			02 Set CSS on Sliderdiv
+ 			03 Set CSS on slides
+ 			04 Paging mechanism
+ 	
+ 	*/
+ 
+    $.fn.slidify = function(options) {
+        
+        /* 
+        
+	    PLUGIN SETTINGS / OPTIONS
+        
+        */
+        
+        var settings = $.extend({
+            // These are the defaults.
+            sliderClass:"slider"
+            sliderHeight:$(this).height(),
+            sliderWidth:$(this).width(),
+            slideSelector: ".slide",
+            clickActive:true
+        }, options );        
+        
+        /* 
+        
+	    CONTAIN SLIDER
+	    Create Parent Container Div of fixed width and overflow hidden
+        
+        */
+    
+        $(this).wrap('<div class="'+settings.sliderClass+'" />');
+
+	    	$('.Slider').css({
+	    		"overflow":"hidden",
+	    		"height":settings.sliderHeight,
+	    		"width":settings.sliderWidth
+	    	});
+        
+
+        /*
+        
+        MODIFY CSS OF SLIDER
+        Set Attr & Width of Slider (this) to total length of children
+        
+		*/
+        
+    	// Get width from children length
+		var numSlides 		= $(settings.slideSelector, $(this)).length;
+		var containerWidth 	= settings.sliderWidth * numSlides; 
+		
+		// Set Attr: position relative & width
+    	$(this).css({
+        	"position":"relative",
+        	"width":containerWidth
+    	});
+        
+        /* 
+        
+        CHILDREN CSS
+        Set required CSS on children
+        
+        */
+        
+        $(settings.slideSelector, $(this)).css({
+	        "float":"left"
+		});
+        
+        /*
+        
+        Paging mechanism
+        
+        */
+        
+        // Initialise index (default is slide 1)
+		var index = 0;
+		
+		// Click
+		if(settings.clickActive) {
+			$(this).on("click", function() {
+				index++; if(index==numSlides) index=0;
+				$(this).animate({
+					"margin-left":-index * settings.sliderWidth
+				});
+			});
+		}
+		
+		// RETURN        
+        return this;
+    };
+ 
+ 
+}( jQuery ));
